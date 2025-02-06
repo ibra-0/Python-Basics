16f2bd2243625c868273e5112d16168ed5f4bcb7
Razyyev Ibrakhim <i_razyyev@kbtu.kz>
2025-02-06 10:36:41 +0500

    Обновление

--- python_variables.py
+++ python_variables.py
@@ -1,4 +1,4 @@
-"""x = 5
+"""""x = 5
 y = "John"
 print(x)
 print(y)"""
@@ -9,7 +9,63 @@ x = "Sally"
 print(x)"""
 
 
-x = 5.0
+"""x = 5.0
 y = "John"
 print(type(x))
 print(type(y))
+"""
+
+
+"""x, y, z = "Orange", "Banana", "Cherry"
+print(x)
+print(y)
+print(z)"""
+
+"""x = y = z = "Orange"
+print(x)
+print(y)
+print(z)"""
+
+"""
+fruits = ["apple", "banana", "cherry"]
+x, y, z = fruits
+print(x)
+print(y)
+print(z)
+"""
+
+
+"""
+x = "Python "
+y = "is "
+z = "awesome"
+print(x+y+z)"""
+
+
+"""x = "awesome"
+
+def myfunc():
+  print("Python is " + x)
+
+myfunc()"""
+
+"""x = "awesome"
+
+def myfunc():
+  x = "fantastic"
+  print("Python is " + x)
+
+myfunc()
+
+print("Python is " + x)
+"""
+
+x = "awesome"
+
+def myfunc():
+  global x
+  x = "fantastic"
+
+myfunc()
+
+print("Python is " + x)
