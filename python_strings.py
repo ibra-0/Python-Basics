3114e4ea69c0f72379b4ab4a11b50f5d5d2faf2f
Razyyev Ibrakhim <i_razyyev@kbtu.kz>
2025-02-06 11:14:40 +0500

    Обновление

---
+++ python_strings.py
@@ -0,0 +1,53 @@
+'''print("It's alright")
+print("He is called 'Johnny'")
+print('He is called "Johnny"')'''
+
+
+'''a = """Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua."""
+print(a)'''
+
+
+'''a = "Hello, World!"
+print(a[8])'''
+
+
+'''for x in "banana":
+  print(x)
+a = "Hello, World!"
+print(len(a))
+'''
+'''a = """Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua."""
+print("free" in a)'''
+
+'''b = "Hello, World!"
+print(b[-5:-2])'''
+
+'''a = "      Hello, World!      "
+print(a.replace("e", "P"))'''
+
+'''a = "Lorem ipsum dolor sit amet,consectetur adipiscing elit,sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+print(a.split(","))'''
+
+'''a = "Hello"
+b = "World"
+c = a + " " + b
+print(c)'''
+
+''''price = 3600000
+txt = f"This bmw m5 competition price is {price} dollars"
+print(txt)'''
+
+'''price = 59
+txt = f"The price is {price:.2f} dollars"
+print(txt)
+'''
+
+txt = "We are the so-called \"Vikings\" from the \ooo north."
+print(txt)
+
