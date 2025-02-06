f5c9608f164adcd983e2769537a5562114b1fdbc
Razyyev Ibrakhim <i_razyyev@kbtu.kz>
2025-02-06 13:57:00 +0500

    Обновление

---
+++ boleanValues.py
@@ -0,0 +1,40 @@
+'''print(10 > 9)
+print(10 == 9)
+print(10 < 9)'''
+
+'''a = 200
+b = 33
+
+if b > a:
+  print("b is greater than a")
+else:
+  print("b is not greater than a")
+  '''
+'''print(bool("Hello"))
+print(bool(6))'''
+
+'''print(bool(False))
+print(bool(None))
+print(bool(0))
+print(bool(""))
+print(bool(()))
+print(bool([]))
+print(bool({}))'''
+
+'''class myclass():
+  def __len__(self):
+    return 0
+
+myobj = myclass()
+print(bool(myobj))'''
+
+"""def myFunction() :
+  return True
+
+if myFunction():
+  print("YES!")
+else:
+  print("NO!")"""
+
+x = "True"
+print(isinstance(x, str))
---
+++ pythonDataTypes.py
@@ -0,0 +1,24 @@
+"""x = 1    
+y = 2.8  
+z = 1j   
+
+a = float(x)
+
+b = int(y)
+
+c = int(x)
+
+print(a)
+print(b)
+print(c)
+
+print(type(a))
+print(type(b))
+print(type(c))"""
+
+
+import random
+
+print(random.randrange(1, 10))
+
+
