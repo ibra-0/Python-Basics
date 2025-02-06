6bab8c3ba8513dc4c453a1f007d117010f420116
Razyyev Ibrakhim <i_razyyev@kbtu.kz>
2025-02-06 15:15:24 +0500

    Обновление

--- pythonList.py
+++ pythonList.py
@@ -1,5 +1,44 @@
-thislist = ["apple", "banana", "cherry", "apple", "cherry"]
+'''thislist = ["apple", "banana", "cherry", "apple", "cherry"]
 print(thislist)
 
 thislist = ["apple", "banana", "cherry"]
-print(len(thislist))
+print(len(thislist))'''
+
+'''list1 = ["apple", "banana", "cherry"]
+list2 = [1, 5, 7, 9, 3]
+list3 = [True, False, False]
+print(list1,list2,list3)'''
+
+'''mylist = ["apple", "banana", "cherry"]
+print(type(mylist))'''
+
+'''thislist = list(("apple", "banana", "cherry")) 
+print(thislist)
+
+thislist = list(("apple", "banana", "cherry")) # note the double round-brackets
+print(thislist[1])
+
+
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(thislist[2:5])'''
+
+'''thislist = ["apple", "banana", "cherry"]
+if "apple" in thislist:
+  print("Yes, 'apple' is in the fruits list")'''
+  
+'''thislist = ["apple", "banana", "cherry", "orange", "kiwi", "mango"]
+thislist[1:3] = ["blackcurrant", "watermelon"]
+print(thislist)'''
+
+thislist = ["apple", "banana", "cherry"]
+thislist.insert(2, "watermelon")
+print(thislist)
+
+thislist = ["apple", "banana", "cherry"]
+thislist.append("orange")
+print(thislist)
+
+thislist = ["apple", "banana", "cherry"]
+tropical = ["mango", "pineapple", "papaya"]
+thislist.extend(tropical)
+print(thislist)
